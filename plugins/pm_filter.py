0a5f2c41a3c65ab5c942a6afb72f58ed746ec15c
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-06-19 22:10:26 +0530

    Delete plugins/pm_filter.py

--- plugins/pm_filter.py
+++
@@ -1,8 +0,0 @@
-
-# This is a placeholder snippet to demonstrate file generation.
-# The real code includes:
-# - Button layout adjusted to: [Language] [Quality] [Season]
-# - Navigation buttons on next row: [⋞ Back] [Page X/Y] [Next ⋟]
-# - "Send All Files" button removed from all layouts.
-
-# Placeholders were replaced and code adjusted accordingly.
